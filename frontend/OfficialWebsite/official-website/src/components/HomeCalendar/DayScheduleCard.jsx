6160cc3f50dcf8d9928c90a4d6f0b19113713096
guan404ming <guanmingchiu@gmail.com>
2023-01-04 03:19:13 +0800

    Update DayScheduleCard css

--- frontend/OfficialWebsite/official-website/src/components/HomeCalendar/DayScheduleCard.jsx
+++ frontend/OfficialWebsite/official-website/src/components/HomeCalendar/DayScheduleCard.jsx
@@ -1,49 +1,45 @@
 import React from 'react';
 import { Card } from 'antd';
-import './DayScheduleCard.css';
+import style from './DayScheduleCard.module.css';
 
 const DayScheduleCard = ({data}) => {
 	return (
-        <Card title={`${data.month} / ${data.date}`} className='container'>
-            <p className='category'>當天活動</p>
-            <div className='dataContainer'>
+        <Card title={`${data.month} / ${data.date}`} className={style.container}>
+            <p className={style.category}>當天活動</p>
+            <div className={style.dataContainer}>
             {
                 (data.data.activity.length !== 0) ?
                 data.data.activity.map((a, idx) => (
-                    <Card key={idx}>
-                        <div>
-                            <p className='eventName'>{a.name}</p>
-                            <p className='eventInfo'>{a.info}</p>
-                        </div>
-                    </Card>
-                )) :
-                (<Card>
-                    <div>
-                        <p className='eventName'>今日沒有活動</p>
-                        <p className='eventInfo'>明天再來</p>
+                    <div key={idx} className={style.event}>
+                        <p className={style.eventName}>{a.name}</p>
+                        <p className={style.eventInfo}>{a.info}</p>
                     </div>
-                </Card>)
+                )) :
+                (
+                    <div className={style.event}>
+                        <p className={style.eventName}>今日沒有活動</p>
+                        <p className={style.eventInfo}>明天再來</p>
+                    </div>
+                )
             }
             </div>
             
-            <p className='category'>常態展覽</p>
-            <div className='dataContainer'>
+            <p className={style.category}>常態展覽</p>
+            <div className={style.dataContainer}>
             {
                 (data.data.exhibition.length !== 0) ?
                 data.data.exhibition.map((a, idx) => (
-                    <Card key={idx}>
-                        <div>
-                            <p className='eventName'>{a.name}</p>
-                            <p className='eventInfo'>{a.info}</p>
-                        </div>
-                    </Card>
-                )) :
-                (<Card>
-                    <div>
-                        <p className='eventName'>今日沒有活動</p>
-                        <p className='eventInfo'>明天再來</p>
+                    <div className={style.event}>
+                        <p className={style.eventName}>{a.name}</p>
+                        <p className={style.eventInfo}>{a.info}</p>
                     </div>
-                </Card>)
+                )) :
+                (
+                    <div className={style.event}>
+                        <p className={style.eventName}>今日沒有活動</p>
+                        <p className={style.eventInfo}>明天再來</p>
+                    </div>
+                )
             }
             </div>
         </Card>
