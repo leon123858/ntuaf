ef23376f3bbb0673bae6298f0abb412c73d46e67
dsa66253 <dsa66253@yahoo.com.tw>
2023-01-04 15:41:03 +0800

    change to css module

--- frontend/OfficialWebsite/official-website/src/components/Footer/Footer.js
+++ frontend/OfficialWebsite/official-website/src/components/Footer/Footer.js
@@ -1,23 +1,23 @@
-import "./Footer.css"
+import styles from "./Footer.module.css"
 import {InstagramOutlined, FacebookOutlined} from '@ant-design/icons';
 
 const Footer = ()=>{
 
     return (
-        <div className="footerWrapper">
-            <div className="title">
-                <span className="logo">LOGO</span>
+        <div className={styles.footerWrapper}>
+            <div className={styles.title}>
+                <span className={styles.logo}>LOGO</span>
                 <h2>台大藝術季28th</h2>
                 
             </div>
             <hr></hr>
-            <div className="infoWrapper">
+            <div className={styles.infoWrapper}>
                 <div>
                 <h3>聯絡我們</h3>
                 <p>羅斯福路</p>
                 <p>ntuartfest@gmail</p>
                 </div>
-                <div className="iconGroup">
+                <div className={styles.iconGroup}>
                     <a href="https://www.instagram.com/"><InstagramOutlined/></a>
                     <a href="https://www.facebook.com/"><FacebookOutlined /></a>
                 </div>
