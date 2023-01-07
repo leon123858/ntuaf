e32443e41a5d1e98657ae426c07f8b10d87f26b5
dsa66253 <dsa66253@yahoo.com.tw>
2023-01-07 22:35:42 +0800

    finish pc RWD

--- frontend/OfficialWebsite/official-website/src/components/Footer/Footer.js
+++ frontend/OfficialWebsite/official-website/src/components/Footer/Footer.js
@@ -1,10 +1,15 @@
 import styles from "./Footer.module.css"
-import {InstagramOutlined, FacebookOutlined} from '@ant-design/icons';
+import {InstagramOutlined, FacebookOutlined, ArrowUpOutlined} from '@ant-design/icons';
 
 const Footer = ()=>{
-
+    function topFunction() {
+        window.scrollTo({top: 0, behavior: 'smooth'});
+        // document.body.scrollTop = 0; // For Safari
+        // document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+    }
     return (
         <div className={styles.footerWrapper}>
+            <ArrowUpOutlined onClick={topFunction} className={styles.top} size="big"/>
             <div className={styles.title}>
                 <span className={styles.logo}>LOGO</span>
                 <h2>台大藝術季28th</h2>
@@ -13,15 +18,17 @@ const Footer = ()=>{
             <hr></hr>
             <div className={styles.infoWrapper}>
                 <div>
-                <h3>聯絡我們</h3>
-                <p>羅斯福路</p>
-                <p>ntuartfest@gmail</p>
+                    <h3>聯絡我們</h3>
+                    <span>羅斯福路</span>
+                    <span>ntuartfest@gmail</span>
                 </div>
                 <div className={styles.iconGroup}>
                     <a href="https://www.instagram.com/"><InstagramOutlined/></a>
                     <a href="https://www.facebook.com/"><FacebookOutlined /></a>
                 </div>
+                
             </div>
+            
         </div>
     )
 }
