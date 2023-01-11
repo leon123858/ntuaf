b9b6db50ab584c3eddf84cc176fe61944b9a111e
guan404ming <guanmingchiu@gmail.com>
2023-01-11 22:59:50 +0800

    Revert "Adjust topic margin"
    This reverts commit 0cedc1086551e3dc2ad2f4c8f1443a3f84ba6a8e.

--- frontend/OfficialWebsite/official-website/src/routers/Home.jsx
+++ frontend/OfficialWebsite/official-website/src/routers/Home.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import style from './Home.module.css';
-import ImageList from '../components/ImageList/ImageList';
+import HomeCalendar from '../components/HomeCalendar/HomeCalendar';
 
 function Home() {
 	return (
@@ -20,7 +20,7 @@ function Home() {
 						Learn React
 					</a>
 				</header> */}
-				<ImageList />
+				<HomeCalendar />
 			</div>
 		</>
 	);
