5eea72c77beb199bf63d0be9b3dbf7a73e1310f3
guan404ming <guanmingchiu@gmail.com>
2023-01-15 00:04:32 +0800

    Revise Data Structure and Layout

--- frontend/OfficialWebsite/official-website/src/components/MemberTab/MemberTab.jsx
+++ frontend/OfficialWebsite/official-website/src/components/MemberTab/MemberTab.jsx
@@ -1,6 +1,8 @@
-import React from 'react';
+import React, {useContext} from 'react';
 import style from './MemberTab.module.css';
 import { Tabs } from 'antd';
+import { BreakPointContext } from '../../useBreakPoint';
+
 
 const defaultMembers = [
     {
@@ -8,27 +10,23 @@ const defaultMembers = [
         departmentImage: 'https://i.seadn.io/gcs/files/74e5cd691632cad888b4f615827ac5f2.png?auto=format',
         members: [
             {
-                section: '總招',
+                position: '總招',
                 name: 'Theo',
             },
             {
-                section: '總招',
+                position: '副招',
                 name: 'Theo',
             },
             {
-                section: '總招',
+                position: '副招',
                 name: 'Theo',
             },
             {
-                section: '總招',
+                position: '副招',
                 name: 'Theo',
             },
             {
-                section: '總招',
-                name: 'Theo',
-            },
-            {
-                section: '總招',
+                position: '執秘',
                 name: 'Theo',
             },
         ],
@@ -38,64 +36,76 @@ const defaultMembers = [
         departmentImage: 'https://i.seadn.io/gae/ZGsGPKiUHiQx2cosXFVUWw4K7YgE-FpgmVenPWBe0OfpHZK4AaOwCvkfT8SlJZx0BujDl5zOYI7dPPN46xCWsMDSm_1F9fKn_TtlbQ?auto=format',
         members: [
             {
-                section: '數位組',
-                name: '邱冠銘',
+                position: '部長',
+                name: '策展',
             },
             {
-                section: '數位組',
-                name: 'Kobe Mary',
+                position: '部員',
+                name: '策展',
             },
             {
-                section: '數位組',
-                name: '李汶凌',
+                position: '部員',
+                name: '策展',
             },
             {
-                section: '數位組',
-                name: '胡海晴',
+                position: '部員',
+                name: '策展',
             },
             {
-                section: '數位組',
-                name: '陳翰文',
+                position: '部員',
+                name: '策展',
             },
             {
-                section: '數位組',
-                name: '林俊佑',
+                position: '部員',
+                name: '策展',
             },
             {
-                section: '活動組',
-                name: 'xxx',
+                position: '部員',
+                name: '策展',
             },
             {
-                section: '活動組',
-                name: 'xxx',
+                position: '部員',
+                name: '策展',
             },
             {
-                section: '活動組',
-                name: 'xxx',
+                position: '部員',
+                name: '策展',
             },
             {
-                section: '活動組',
-                name: 'xxx',
+                position: '部員',
+                name: '策展',
             },
             {
-                section: '展覽組',
-                name: 'xxx',
+                position: '部員',
+                name: '策展',
             },
             {
-                section: '展覽組',
-                name: 'xxx',
+                position: '部員',
+                name: '策展',
             },
             {
-                section: '展覽組',
-                name: 'xxx',
+                position: '部員',
+                name: '策展',
             },
             {
-                section: '展覽組',
-                name: 'xxx',
+                position: '部員',
+                name: '策展',
             },
             {
-                section: '展覽組',
-                name: 'xxx',
+                position: '部員',
+                name: '策展',
+            },
+            {
+                position: '部員',
+                name: '策展',
+            },
+            {
+                position: '部員',
+                name: '策展',
+            },
+            {
+                position: '部員',
+                name: '策展',
             },
         ],
     },
@@ -104,36 +114,36 @@ const defaultMembers = [
         departmentImage: 'https://i.seadn.io/gcs/files/a8b3e484f6ec637b551f727255364479.png?auto=format',
         members: [
             {
-                section: 'xx組',
-                name: '行政部的人',
+                position: '部長',
+                name: '行政',
             },
             {
-                section: 'xx組',
-                name: '行政部的人',
+                position: '部員',
+                name: '行政',
             },
             {
-                section: 'xx組',
-                name: '行政部的人',
+                position: '部員',
+                name: '行政',
             },
             {
-                section: 'xx組',
-                name: '行政部的人',
+                position: '部員',
+                name: '行政',
             },
             {
-                section: 'xx組',
-                name: '行政部的人',
+                position: '部員',
+                name: '行政',
             },
             {
-                section: 'xx組',
-                name: '行政部的人',
+                position: '部員',
+                name: '行政',
             },
             {
-                section: 'xx組',
-                name: '行政部的人',
+                position: '部員',
+                name: '行政',
             },
             {
-                section: 'xx組',
-                name: '行政部的人',
+                position: '部員',
+                name: '行政',
             },
         ],
     },
@@ -142,36 +152,36 @@ const defaultMembers = [
         departmentImage: 'https://i.seadn.io/gae/tru7H_Rgi1tFz_X7ke3OZp-xOYUh2WdifeS0-HSQmZBgOGt9zY64At9364GXXHUxBPuS8gz3eFTNo2xnVgp_D8h_vFKvwy_Va6Wo?auto=format',
         members: [
             {
-                section: 'xx組',
-                name: '公關部的人',
+                position: '部長',
+                name: '公關',
             },
             {
-                section: 'xx組',
-                name: '公關部的人',
+                position: '部員',
+                name: '公關',
             },
             {
-                section: 'xx組',
-                name: '公關部的人',
+                position: '部員',
+                name: '公關',
             },
             {
-                section: 'xx組',
-                name: '公關部的人',
+                position: '部員',
+                name: '公關',
             },
             {
-                section: 'xx組',
-                name: '公關部的人',
+                position: '部員',
+                name: '公關',
             },
             {
-                section: 'xx組',
-                name: '公關部的人',
+                position: '部員',
+                name: '公關',
             },
             {
-                section: 'xx組',
-                name: '公關部的人',
+                position: '部員',
+                name: '公關',
             },
             {
-                section: 'xx組',
-                name: '公關部的人',
+                position: '部員',
+                name: '公關',
             },
         ],
     },
@@ -180,60 +190,90 @@ const defaultMembers = [
         departmentImage: 'https://i.seadn.io/gcs/files/9d1db9251715abbf63586037840a319c.png?auto=format',
         members: [
             {
-                section: 'xx組',
-                name: '設計部的人',
+                position: '部長',
+                name: '設計',
             },
             {
-                section: 'xx組',
-                name: '設計部的人',
+                position: '部員',
+                name: '設計',
             },
             {
-                section: 'xx組',
-                name: '設計部的人',
+                position: '部員',
+                name: '設計',
             },
             {
-                section: 'xx組',
-                name: '設計部的人',
+                position: '部員',
+                name: '設計',
             },
             {
-                section: 'xx組',
-                name: '設計部的人',
+                position: '部員',
+                name: '設計',
             },
             {
-                section: 'xx組',
-                name: '設計部的人',
+                position: '部員',
+                name: '設計',
             },
             {
-                section: 'xx組',
-                name: '設計部的人',
+                position: '部員',
+                name: '設計',
             },
             {
-                section: 'xx組',
-                name: '設計部的人',
+                position: '部員',
+                name: '設計',
             },
         ],
     }
 ]
 
+const PositionRow = ({department, position}) => {
+    return (
+        <div className={style.positionRow}>
+            <p style={{ margin: 0 }}>{position}</p>
+            <div className={style.memberContainer}>
+                {department.members.map((member, idx) => (
+                    (member.position === position)
+                    ? (
+                        <div key={idx} className={style.member}>
+                            <span>{member.name}</span>
+                        </div>
+                    ) : ''
+                ))}
+            </div>
+        </div>
+    )
+}
+
 const MemberTab = ({members = defaultMembers}) => {
+    const { inBreakPoint } = useContext(BreakPointContext);
     const handleProcess = (department, idx) => {
         return {
             label: department.departmentName,
             key: idx,
-            children:
-                (<div className={style.department}>
-                    <div className={style.imageContainer}>
-                        <img alt='team img' src={department.departmentImage} />
-                    </div>
-                    <div className={style.memberContainer}>
-                        {department.members.map((member, idx) => (
-                            <div key={idx} className={style.member}>
-                                <span className={style.memberName}>{member.section}</span>
-                                <span>{member.name}</span>
+            children: (
+                <div className={(inBreakPoint) ? style.department : style.lgDepartment}>                    
+                    <img 
+                        alt='team img' 
+                        src={department.departmentImage} 
+                        className={(inBreakPoint) ? style.img : style.lgImg} 
+                    />
+                    {
+                        (department.departmentName === '核心團隊')
+                        ? (
+                            <div>
+                                <PositionRow department={department} position={"總招"} />
+                                <PositionRow department={department} position={"副招"} />
+                                <PositionRow department={department} position={"執秘"} />
                             </div>
-                        ))}
-                    </div>
-                </div>),
+                        )
+                        : (
+                            <div>
+                                <PositionRow department={department} position={"部長"} />
+                                <PositionRow department={department} position={"部員"} />
+                            </div>
+                        )
+                    } 
+                </div>
+            ),
         }
     };
 
@@ -242,10 +282,10 @@ const MemberTab = ({members = defaultMembers}) => {
             <Tabs
                 centered
                 defaultActiveKey="1"
+                size='large'
                 items={members.map((department, idx) => handleProcess(department, idx))}>
             </Tabs>
         </div>
     );
 };
 export default MemberTab;
-
