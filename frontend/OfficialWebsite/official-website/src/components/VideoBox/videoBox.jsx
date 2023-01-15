b76cf31f48257cbbeb7a21c0bc666ee773f4678f
Cedricyu <86541569+Cedricyu@users.noreply.github.com>
2023-01-15 16:26:13 +0800

    add videobox

---
+++ frontend/OfficialWebsite/official-website/src/components/VideoBox/videoBox.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+// import style from './Home.module.css';
+
+
+const Video = () => {
+	return(
+		<iframe width="560" height="315" src="https://www.youtube.com/embed/YI88ca_jzSs" title="YouTube video player" frameborder="1" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+	);
+}
+
+
+export default Video;
--- frontend/OfficialWebsite/official-website/src/routers/About.jsx
+++ frontend/OfficialWebsite/official-website/src/routers/About.jsx
@@ -1,51 +1,26 @@
 import React from 'react';
-// import style from './Home.module.css';
-import {Card,Tag,Image} from 'antd';
-
-const { Meta } = Card;
-
-const Event = () => {
-	return(
-		<div style={{position: 'relative',top:'-20px', left: '-20px'}}>
-			<div style={{position: 'relative'}}>
-				<Image alt="example" src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png" />
-				<div style={{position: 'relative', bottom: '25px', left: '0'}}>
-					<Tag color="magenta" >工作仿</Tag>
-				</div>	
-			</div>
-		</div>
-		
-	)
-}
+import style from './Home.module.css';
 
 function About() {
 	return (
-		<Card
-			hoverable
-			style={{ width: 240,position: 'relative' }}
-			cover= {<Event/>}
-		>
-			<Meta title="Europe Street beat" description="www.instagram.com" />
-		</Card>
-		// <>
-		// 	<div className={style.APP}>
-		// 		<header className={style['App-header']}>
-		// 			<img src={'/logo.svg'} className={style['App-logo']} alt='logo' />
-		// 			<p>
-		// 				Edit <code>src/App.js</code> and save to reload.
-		// 			</p>
-		// 			<a
-		// 				className={style['App-link']}
-		// 				href='https://reactjs.org'
-		// 				target='_blank'
-		// 				rel='noopener noreferrer'
-		// 			>
-		// 				Learn React
-		// 			</a>
-		// 		</header>
-		// 	</div>
-		// </>
-
+		<>
+			<div className={style.APP}>
+				<header className={style['App-header']}>
+					<img src={'/logo.svg'} className={style['App-logo']} alt='logo' />
+					<p>
+						Edit <code>src/App.js</code> and save to reload.
+					</p>
+					<a
+						className={style['App-link']}
+						href='https://reactjs.org'
+						target='_blank'
+						rel='noopener noreferrer'
+					>
+						Learn React
+					</a>
+				</header>
+			</div>
+		</>
 	);
 }
 
