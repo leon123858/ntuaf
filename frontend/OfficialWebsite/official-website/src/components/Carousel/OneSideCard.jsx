55c6b84e5fd91319a76840aa2e817b94ca5c270c
dsa66253 <dsa66253@yahoo.com.tw>
2023-01-15 23:41:32 +0800

    adjust rwd

--- frontend/OfficialWebsite/official-website/src/components/Carousel/OneSideCard.jsx
+++ frontend/OfficialWebsite/official-website/src/components/Carousel/OneSideCard.jsx
@@ -4,8 +4,11 @@ const OneSideCard = ({cardContent}) => {
     return (
         <div className={style.oneSideCard}>
             {/* <a href='https://google.com' style={{textDecoration:"none", color:"black"}}> */}
-            <img src='https://images.squarespace-cdn.com/content/v1/5452d441e4b0c188b51fef1a/1615326541809-TW01PVTOJ4PXQUXVRLHI/male-orange-tabby-cat.jpg?format=300w'
+            <div className={style.imageWrapper}>
+                <img src='https://images.squarespace-cdn.com/content/v1/5452d441e4b0c188b51fef1a/1615326541809-TW01PVTOJ4PXQUXVRLHI/male-orange-tabby-cat.jpg?format=600w'
                 alt="here"></img>
+            </div>
+            
             <h4>{cardContent.header}</h4>
             <p className={style.disc}>{cardContent.content} {cardContent.index}</p>
             {/* </a> */}
