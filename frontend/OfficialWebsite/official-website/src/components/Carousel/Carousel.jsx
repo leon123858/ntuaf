c76ed73e3f4bc5da647014c95fe1b1430cc55869
dsa66253 <dsa66253@yahoo.com.tw>
2023-01-15 23:01:36 +0800

    seperate component

--- frontend/OfficialWebsite/official-website/src/components/Carousel/Carousel.jsx
+++ frontend/OfficialWebsite/official-website/src/components/Carousel/Carousel.jsx
@@ -2,158 +2,20 @@ import { useState } from "react"
 import style from "./Carousel.module.css"
 import { useSwipeable } from "react-swipeable"
 import {default as CarouselImport}  from 'react-spring-3d-carousel';
-import { Card as CardAntd, Tabs } from 'antd';
-
-const { Meta } = CardAntd;
-// import { uuid } from 'uuidv4';
-const OneSideCard = ({cardContent}) => {
-    // console.log(cardContent)
-    return (
-        <div className={style.oneSideCard}>
-            {/* <a href='https://google.com' style={{textDecoration:"none", color:"black"}}> */}
-            <img src='https://images.squarespace-cdn.com/content/v1/5452d441e4b0c188b51fef1a/1615326541809-TW01PVTOJ4PXQUXVRLHI/male-orange-tabby-cat.jpg?format=300w'
-                alt="here"></img>
-            <h4 className={style.title}>{cardContent.header}</h4>
-            <p className={style.disc}>{cardContent.content} {cardContent.index}</p>
-            {/* </a> */}
-        </div>
-    )
-}
-const DoubleSideCard = ({FrontCardContent, BackCardContent, flip}) => {
-    // console.log(flip, flip===true, [style.dCardInnerWrapper, style.dCardInnerWrapperFlip].join(' '))
-    // let cardContent0 = frontCard
-    // let cardContent1 = backCard
-    console.log("FrontCard", )
-    return (
-        <div className={style.dCardWrapper}>
-            <div className={flip===true?[style.dCardInnerWrapper, style.dCardInnerWrapperFlip].join(' '):style.dCardInnerWrapper}>
-                <div className={style.flipFrontCard}><OneSideCard cardContent={FrontCardContent}/></div>
-                {/* {FrontCard?<FrontCard/>:<></>} */}
-                {/* {BackCard?<BackCard/>:<></>} */}
-                <div className={style.flipBackCard}><OneSideCard cardContent={BackCardContent}/></div>
-            </div>
-        </div>
-    )
-}
-const CarouselCard2 = ({activateImg, cardList, flip}) => {
-    // let {recentActivity, regularExpo} = cardContents
-    let frontCard = cardList[0]
-    let backCard =  cardList[0]
-    let translatePercent = -cardList[0].index * 50 - (activateImg - 1) * 50
-    let opacity = 1
-    let scale = 1
-    if (cardList[0].index - activateImg === 1 || cardList[0].index - activateImg === -1) {
-        // it seems like flip can be set here
-        opacity = 0.5
-        scale = 0.7
-    } else if (cardList[0].index - activateImg > 1 || cardList[0].index - activateImg < -1) {
-        opacity = 0
-        scale = 0.7
-    }
-
-    return (
-    <div className={cardList[0].index === activateImg ? [style.activeSlide, style.slide].join(' ') : style.slide} style={{ transform: `translateX(${translatePercent}%) scale(${scale})`, opacity: `${opacity}`, }}>
-        {/* <a href='https://google.com' style={{textDecoration:"none", color:"black"}}> */}
-            <DoubleSideCard frontCard={frontCard} backCard={backCard} flip={false}/>
-        {/* </a> */}
-    </div>
-    )
-}
-
-const CarouselCard = ({active, cardList, flip, activateImg}) => {
-    // let {recentActivity, regularExpo} = cardContents
-    let frontCard = cardList[0]
-    let backCard =  cardList[0]
-    let translatePercent = -cardList[0].index * 50 - (activateImg - 1) * 50
-    let opacity = 1
-    let scale = 1
-    if (cardList[0].index - activateImg === 1 || cardList[0].index - activateImg === -1) {
-        // it seems like flip can be set here
-        opacity = 0.5
-        scale = 0.7
-    } else if (cardList[0].index - activateImg > 1 || cardList[0].index - activateImg < -1) {
-        opacity = 0
-        scale = 0.7
-    }
-
-    return (
-    <div className={cardList[0].index === activateImg ? [style.activeSlide, style.slide].join(' ') : style.slide} style={{ transform: `translateX(${translatePercent}%) scale(${scale})`, opacity: `${opacity}`, }}>
-        {/* <a href='https://google.com' style={{textDecoration:"none", color:"black"}}> */}
-            <DoubleSideCard frontCard={frontCard} backCard={backCard} flip={false}/>
-        {/* </a> */}
-    </div>
-    )
-}
-const CarouselComponent = ({ cardList }) => {
-    const [activateImg, setActivateImg] = useState(1);
-    const moveLeft = () => {
-        if (activateImg <= 0) {
-            // reach the first image
-            return
-        }
-        setActivateImg((activateImg - 1) % (cardList.length))
-    }
-
-    const moveRight = () => {
-        if (activateImg >= cardList.length - 1) {
-            // reach the last image
-            return
-        }
-        setActivateImg((activateImg + 1) % (cardList.length))
-    }
-    //info swipe effect
-    const handlers = useSwipeable({
-        onSwipedLeft: () => { if (activateImg + 1 <= cardList.length - 1) { setActivateImg((prev) => prev + 1) } },
-        onSwipedRight: () => { if (0 <= activateImg - 1) { setActivateImg((prev) => prev - 1) } },
-    });
-    return (
-        <div className={style.carouselWrapper}  {...handlers}>
-            <div className={style.prev} onClick={moveLeft}>❮</div>
-                <div className={style.next} onClick={moveRight}>❯</div>
-            {/* {cardCotents.map((cardContent, idx) => (
-                        <CarouselCard index={idx} cardContent={cardContent} activateImg={activateImg}/>
-                ))} */}
-
-            {/* {cardContents.map((cardContent, idx) => (
-                <CarouselCard index={idx} cardContent={cardContent} activateImg={activateImg} />
-            ))} */}
-
-        </div>
-    )
-}
-const TestComponent = ({Card})=>{
-    return (<Card content={"content here"}/>)
-}
-const Card = ({content})=>{
-    console.log("Card", content)
-    return(
-        <OneSideCard/>
-    )
-}
-
+import DoubleSideCard from "./DoubleSideCard.jsx"
 
 const Carousel = () => {
+    
     const [activateImg, setActivateImg] = useState(1);
     const [flip, setFlip] = useState(false);
     const moveLeft = () => {
-        // if (activateImg <= 0) {
-        //     // reach the first image
-        //     return
-        // }
         setActivateImg(activateImg-1)
-        // setActivateImg((activateImg - 1) % (cardList.length))
     }
 
     const moveRight = () => {
-        // if (activateImg >= cardList.length - 1) {
-        //     // reach the last image
-        //     return
-        // }
         setActivateImg(activateImg+1)
-        // setActivateImg((activateImg + 1) % (cardList.length))
     }
-    // todo draggable
-    // todo make active image larger in mobile mode
+    //info for sample data
     let aimages = [0, 1, 2, 3, 4]
     let eimages = [0, 1, 2]
     let defaultContent = {
@@ -191,35 +53,21 @@ const Carousel = () => {
             }
         })
     }
-    console.log(activityContentList, evnetContentList)
-    let recentActivity = eimages.map((index) => {return ({...acardContent, index:index})})
-    let regularExpo = eimages.map((index) => {return ({...ecardContent, index:index})})
-    let cardContents = {recentActivity, regularExpo}
-    // console.log(cardContents)
-    const createCards = (content)=>{
-        return(
-            <OneSideCard cardContent={content}/>
-        )
-    }
-    let frontCardL =  createCards(recentActivity)
-    let backCardL =  createCards(regularExpo)
+    //info create carousel cards for carousel
     let slides = activityContentList.map((content, i)=>{
-
         return ({
             key: i,
             content: 
             <div className={style.cardWrapper}>
                 <DoubleSideCard FrontCardContent={activityContentList[i]} BackCardContent={evnetContentList[i]} flip={flip}></DoubleSideCard>
-
             </div>
-        //     content: <CardAntd cover={<img alt="example" src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png" />}>
-        //     <Meta title="Europe Street beat" description="www.instagram.com" />
-        // </CardAntd>
         })
     } )
-
-
-
+    //info swipe effect
+    const handlers = useSwipeable({
+        onSwipedLeft: () => { setActivateImg(activateImg+1) },
+        onSwipedRight: () => { setActivateImg(activateImg-1) },
+    });
 
     return (
         <div className={style.App}>
@@ -228,7 +76,7 @@ const Carousel = () => {
                 <h1 className={style.header} onClick={()=>{setFlip(true)}}>常設展覽</h1>
             </div>
             
-            <div className={style.carouselWrapper} >
+            <div className={style.carouselWrapper}  {...handlers}>
                 <div className={style.prev} onClick={moveLeft}>❮</div>
                 <div className={style.next} onClick={moveRight}>❯</div>
                 <CarouselImport slides={slides} goToSlide={activateImg}></CarouselImport>
---
+++ frontend/OfficialWebsite/official-website/src/components/Carousel/OneSideCard.jsx
@@ -0,0 +1,15 @@
+import style from "./OneSideCard.module.css"
+
+const OneSideCard = ({cardContent}) => {
+    return (
+        <div className={style.oneSideCard}>
+            {/* <a href='https://google.com' style={{textDecoration:"none", color:"black"}}> */}
+            <img src='https://images.squarespace-cdn.com/content/v1/5452d441e4b0c188b51fef1a/1615326541809-TW01PVTOJ4PXQUXVRLHI/male-orange-tabby-cat.jpg?format=300w'
+                alt="here"></img>
+            <h4>{cardContent.header}</h4>
+            <p className={style.disc}>{cardContent.content} {cardContent.index}</p>
+            {/* </a> */}
+        </div>
+    )
+}
+export default OneSideCard
