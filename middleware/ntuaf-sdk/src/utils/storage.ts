6b94cb1bc424ac09345ee51952e59528a751d07e
leon123858 <a0970785699@gmail.com>
2023-01-20 23:05:24 +0800

    upload image OK

--- middleware/ntuaf-sdk/src/utils/storage.ts
+++ middleware/ntuaf-sdk/src/utils/storage.ts
@@ -5,15 +5,18 @@ import { ref, getDownloadURL, uploadBytes } from 'firebase/storage';
 /**
  * 上傳圖片
  * @param url 本機圖片網址
+ * @param skipTest 略過前端驗證 default:false
  * @returns 下載圖片網址
  */
-export const uploadImage = async (url: string) => {
+export const uploadImage = async (url: string, skipTest = false) => {
 	const uid = userId();
 	if (!url || !uid) throw 'should have url and login when upload image';
 	const file = await fetch(url).then((result) => result.blob());
-	if (!['image/png', 'image/jpeg'].includes(file.type))
-		throw 'only can use jpg or png';
-	if (file.size > 25 * 1024 * 1024) throw 'the max file size is 25MB';
+	if (!skipTest) {
+		if (!['image/png', 'image/jpeg'].includes(file.type))
+			throw 'only can use jpg or png';
+		if (file.size > 25 * 1024 * 1024) throw 'the max file size is 25MB';
+	}
 	const reference = ref(
 		storageInstance,
 		`images/${uid}/${new Date().getTime()}`
