bd183968c67aa860d8e8c46c4e0d4afe783c0f68
leon123858 <a0970785699@gmail.com>
2023-01-23 20:07:24 +0800

    add cli-progress sample

--- admin/ntuaf-cli/src/utils/insertSample.ts
+++ admin/ntuaf-cli/src/utils/insertSample.ts
@@ -1,6 +1,9 @@
 import { db } from '../init';
+const cliProgress = require('cli-progress');
 
 const insertSample = async () => {
+	const bar = new cliProgress.SingleBar({}, cliProgress.Presets.shades_classic);
+	bar.start(1, 0);
 	const cityRef = db.collection('cities').doc('BJ');
 	const res = await cityRef.set(
 		{
@@ -8,6 +11,8 @@ const insertSample = async () => {
 		},
 		{ merge: true }
 	);
+	bar.increment(1);
+	bar.stop();
 };
 
 export { insertSample };
