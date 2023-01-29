4e599c89cfbbf04f844f50ad757955aff30039c5
dsa66253 <75064168+dsa66253@users.noreply.github.com>
2023-01-29 22:12:02 +0800

    Feature footer (#40)
    * footer content
    
    * finish footer appearance
    
    * change to css module
    
    * finish pc RWD
    
    * add line icon and YT icon

--- frontend/OfficialWebsite/official-website/src/components/Layout/Layout.jsx
+++ frontend/OfficialWebsite/official-website/src/components/Layout/Layout.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import Header from '../Header/Header';
 import { Outlet } from 'react-router-dom';
+import Footer from "../Footer/Footer"
 
 const Layout = ({ initItem }) => {
 	return (
 		<>
 			<Header initItem={initItem} />
-			<Outlet />
+			{/* <Outlet /> */}
+			<Footer/>
 		</>
 	);
 };
