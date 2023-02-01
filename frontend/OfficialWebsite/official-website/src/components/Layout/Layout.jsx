a795bb427ebb376c8959439725998195204249c0
dsa66253 <75064168+dsa66253@users.noreply.github.com>
2023-02-01 11:47:03 +0800

    Feature carousel (#39)
    * almost done
    
    * finish card
    
    * function properly
    
    * remove unsed depedency
    
    * fix click animation on mobile
    
    * change to css module
    
    * add swipe effect to carousel
    
    * messy code but function well
    
    * seperate component
    
    * adjust rwd
    
    * modify relative size of card
    
    * modify despktop version card size

--- frontend/OfficialWebsite/official-website/src/components/Layout/Layout.jsx
+++ frontend/OfficialWebsite/official-website/src/components/Layout/Layout.jsx
@@ -1,15 +1,17 @@
 import React from 'react';
 import Header from '../Header/Header';
 import { Outlet } from 'react-router-dom';
+import styles from "./Layout.module.css"
 import Footer from "../Footer/Footer"
+
 
 const Layout = ({ initItem }) => {
 	return (
-		<>
+		<div className={styles.layout}>
 			<Header initItem={initItem} />
-			{/* <Outlet /> */}
+			<Outlet />
 			<Footer/>
-		</>
+		</div>
 	);
 };
 export default Layout;
