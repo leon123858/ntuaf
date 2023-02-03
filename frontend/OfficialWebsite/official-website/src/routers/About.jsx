dee6589c5e979a91ba6973f6de26b24122dfb822
Cedricyu <86541569+Cedricyu@users.noreply.github.com>
2023-02-03 20:12:14 +0800

    addjust artBox

--- frontend/OfficialWebsite/official-website/src/routers/About.jsx
+++ frontend/OfficialWebsite/official-website/src/routers/About.jsx
@@ -1,17 +1,10 @@
-// import React from 'react';
-// import style from './Home.module.css';
-import ArtBox from "../components/ArtBox/artBox";
-import Carousel from '../components/Carousel/Carousel'
-
+import React from 'react';
+import style from './Home.module.css';
 
 function About() {
 	return (
 		<>
-			<ArtBox artName = "露娜" artist="諾克提斯" arturl = "https://wallpapercrafter.com/sizes/2048x1152/120189-Final-Fantasy-XV-video-games-Luna-Final-Fantasy-XV-Final-Fantasy.png"
-			height ="10px" width = "30px" imageWidth = "300"
-			/>
-			{/* <Carousel/> */}
-			{/* <div className={style.APP}>
+			<div className={style.APP}>
 				<header className={style['App-header']}>
 					<img src={'/logo.svg'} className={style['App-logo']} alt='logo' />
 					<p>
@@ -26,7 +19,7 @@ function About() {
 						Learn React
 					</a>
 				</header>
-			</div> */}
+			</div>
 		</>
 	);
 }
