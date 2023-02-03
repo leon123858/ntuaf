3162ffb1baa95c4a3fec11f618e5786f6408751d
Cedricyu <86541569+Cedricyu@users.noreply.github.com>
2023-02-03 17:14:00 +0800

    addjust map

--- frontend/OfficialWebsite/official-website/src/components/Map/Map.jsx
+++ frontend/OfficialWebsite/official-website/src/components/Map/Map.jsx
@@ -3,11 +3,15 @@ import React from 'react';
 // import { Card } from 'antd';
 
 
-const Map = () => {
+const Map = (props) => {
+	console.log(props);
+	const { inputValue,width,height,font } = props;
+	console.log(props.width);
 	return(
 		<>
-			<div style={{ textAlign : 'left' , width: 560 ,marginBottom : 20 }}>藝術家地圖</div>
-            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3615.4727012957205!2d121.53607172695311!3d25.018027399999998!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3442a9898c33fb79%3A0x986a86f132e4eae4!2z5rS75aSn6aSQ5buz!5e0!3m2!1szh-TW!2stw!4v1674007111196!5m2!1szh-TW!2stw" width="600" height="450"  allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade" title="myFrame"></iframe>
+	
+				<div style={{ textAlign : 'center',marginBottom : 20, width: 'props.width' }}>藝術家地圖</div>
+				<iframe src={inputValue} width={width} height={height} loading="lazy" referrerpolicy="no-referrer-when-downgrade" title="myFrame"></iframe>
         </>
 	);
 }
