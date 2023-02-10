c35e612ce4de57a6df9926d3a2f22f92c56ef19d
dsa66253 <dsa66253@yahoo.com.tw>
2023-02-10 21:46:39 +0800

    remove unsed commend code

--- admin/ntuaf-cli/src/utils/deleteSample.ts
+++ admin/ntuaf-cli/src/utils/deleteSample.ts
@@ -7,9 +7,7 @@ async function deleteCollection(collectionPath:string) {
   const batchSize = 10;
 
   const snapshot = await db.collection(collectionPath).get();
-  // const snapshot = await docRef
   const tmp = await db.collection('cities').doc('DC')
-  // console.log(snapshot)
   
   snapshot.forEach(async(doc) => {
     if(doc.data().id.includes("(test)_")){
@@ -24,35 +22,4 @@ async function deleteCollection(collectionPath:string) {
 
 }
 
-// async function deleteQueryBatch(db: any, query: any, resolve: any, bar:any) {
-//   const snapshot = await query.get();
-//   const batchSize = snapshot.size;
-//   if (batchSize === 0) {
-//     // When there are no documents left, we are done
-//     resolve();
-//     bar.increment(1);
-//     bar.stop();
-//     return;
-//   }
-
-//   // Delete documents in a batch
-//   const batch = db.batch();
-//   snapshot.docs.forEach((doc: any) => {
-//     console.log(doc.id)
-//     if (doc.id.includes("(test)_")){
-//       batch.delete(doc.ref);
-//     }
-//     else{
-//       console.log("not delete", doc.id)
-//     }
-//     // 
-//   });
-//   await batch.commit();
-
-//   // Recurse on the next process tick, to avoid
-//   // exploding the stack.
-//   process.nextTick(() => {
-//     deleteQueryBatch(db, query, resolve, bar);
-//   });
-// }
 export { deleteCollection };
