1bf28cf9cbdfb819eb60a73f1041bbe5926962b7
dsa66253 <dsa66253@yahoo.com.tw>
2023-02-15 18:10:52 +0800

    feature:prototype fourCircle

--- frontend/OfficialWebsite/official-website/src/components/FourCircle/BackCard.jsx
+++ frontend/OfficialWebsite/official-website/src/components/FourCircle/BackCard.jsx
@@ -1,4 +1,4 @@
-import styles from "./FrontCard.module.css"
+import styles from "./BackCard.module.css"
 import {CloseOutlined} from "@ant-design/icons"
 const BackCard = ({BackCardContent, setFlip, closeModel})=>{
     const handleFlip = () => {
@@ -6,14 +6,16 @@ const BackCard = ({BackCardContent, setFlip, closeModel})=>{
             return !prev
         })
     }
-    return (<div className={styles.frontCardWrapper}>
+    return (
+    <div className={styles.BackCardWrapper}>
         <div className={styles.title}>{BackCardContent.title}</div>
         <div className={styles.closeIconWrapper}><CloseOutlined style={{fontSize:"20px", padding:"20px"}} onClick={()=>{closeModel()}}/></div>
         <div className={styles.textWrapper}>
             <div className={styles.content}>{BackCardContent.content}</div>
-            <div className={styles.condiscriptiontent}>{BackCardContent.discription}</div>
+            <div className={styles.imgWrapper}>IMG</div>
             <div onClick={handleFlip}>Back </div>
         </div>
+        
     </div>)
 }
 export default BackCard
--- frontend/OfficialWebsite/official-website/src/components/FourCircle/FourCircle.jsx
+++ frontend/OfficialWebsite/official-website/src/components/FourCircle/FourCircle.jsx
@@ -1,9 +1,5 @@
 import Card from "./Card.jsx"
 import styles from "./FourCircle.module.css"
-import { useState } from 'react';
-import {FrontCard} from "./FrontCard.jsx"
-import { Button, Modal } from 'antd';
-import DoubleCard from "./DoubleCard.jsx"
 const contents = [
     {
         title: "起",
@@ -31,28 +27,6 @@ const contents = [
     },
 ]
 const FourCircle = () => {
-    // const [isModalOpen, setIsModalOpen] = useState(false);
-    // const showModal = () => {
-    //     setIsModalOpen(true);
-    //   };
-    //   const handleOk = () => {
-    //     setIsModalOpen(false);
-    //   };
-    //   const handleCancel = () => {
-    //     setIsModalOpen(false);
-    //   };
-    // const [isModalOpen, setIsModalOpen] = useState(false);
-
-    // const showModal = () => {
-    //     setIsModalOpen(true);
-    //     // console.log("helo")
-    // };
-    // const handleOk = () => {
-    // setIsModalOpen(false);
-    // };
-    // const handleCancel = () => {
-    // setIsModalOpen(false);
-    // };
     return (
         <>
             <div className={styles.cardWrapper}>
@@ -62,14 +36,6 @@ const FourCircle = () => {
                     )
                 })}
             </div>
-                {/* <Button type="primary" onClick={showModal}>
-        Open Modal
-        </Button>
-                        <Modal title="Basic Modal" open={isModalOpen} onOk={handleOk} onCancel={handleCancel}>
-        <p>Some contents...</p>
-        <p>Some contents...</p>
-        <p>Some contents...</p>
-      </Modal> */}
             
         </>)
 }
