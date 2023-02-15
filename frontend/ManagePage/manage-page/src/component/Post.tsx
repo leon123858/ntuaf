7d7e45a61122086c9b68cef4554f1017bd2785a5
ZiJason <jasonnosajya53@gmail.com>
2023-02-15 22:34:43 +0800

    無限接近完成

--- frontend/ManagePage/manage-page/src/component/Post.tsx
+++ frontend/ManagePage/manage-page/src/component/Post.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { Avatar, Divider, List, Skeleton, Card, Tabs } from 'antd';
+import { Avatar, Divider, List, Skeleton, Card, Tabs, Button } from 'antd';
 import InfiniteScroll from 'react-infinite-scroll-component';
 import { getArtworkList, getMembersByDepartment } from '@leon123858/ntuaf-sdk'
 import { ARTWORK_TYPE, Artwork } from '@leon123858/ntuaf-sdk';
@@ -8,55 +8,60 @@ function Post() {
 	const [dataList, setDataList] = useState<Artwork[]>([])
 	const [loading, setLoading] = useState(false);
 	const [cursor, setCursor] = useState<any>();
-	const [activeKey, setActiveKey] = useState("1")
+	const [activeKey, setActiveKey] = useState("3")
 
 	useEffect(() => {
-
-		(async () => {
-			initData()
-			// let { cursor: tempCursor, data } = await getArtworkList(ARTWORK_TYPE.PAINTING, "createTime");
-			// console.log(data)
-			// setDataList(data)
-			// setCursor(tempCursor)
-		})()
-	}, []);
-	useEffect(() => {
-		console.log("cursor:", cursor);
-		console.log("data:", dataList);
-		(async () => {
-			initData()
-			// let { cursor: tempCursor, data } = await getArtworkList(ARTWORK_TYPE.PAINTING, "createTime");
-			// console.log(data)
-			// setDataList(data)
-			// setCursor(tempCursor)
-		})()
-	}, [activeKey]);
-	const loadMoreData = () => {
-		alert("update");
 		if (loading) {
 			console.log("return")
 			return;
 		}
 		setLoading(true);
-		
 		(async () => {
-			updateData();
-			setLoading(false);
-		})()
+			await initData()
+			
+		})().then(()=>{setLoading(false)})
+		
+	}, []);
+	useEffect(() => {
+		console.log("cursor:", cursor);
+		console.log("data:", dataList);
+		setDataList([]);
+		setCursor(1);
+		if (loading) {
+			console.log("return")
+			return;
+		}
+		setLoading(true);
+		(async () => {
+			await initData().then(() => { console.log("initS") })
+		})().then(()=>{setLoading(false)})
+	}, [activeKey]);
+	const loadMoreData = () => {
+		console.log("loadMore")
+		if (loading) {
+			console.log("return")
+			return;
+		}
+		setLoading(true);
+
+		(async () => {
+			await updateData().then(() => { console.log("updateS") });
+		})().then(()=>{setLoading(false)})
 	};
+
 	const updateData = async () => {
-		const type = activeKey==="1"?ARTWORK_TYPE.PURE_TEXT:activeKey==="2"?ARTWORK_TYPE.PHOTO:ARTWORK_TYPE.PAINTING;
+		const type = activeKey === "1" ? ARTWORK_TYPE.PURE_TEXT : activeKey === "2" ? ARTWORK_TYPE.PHOTO : ARTWORK_TYPE.PAINTING;
 		let { cursor: tempCursor, data } = await getArtworkList(type, 'like', cursor);
 		setDataList([...dataList, ...data])
 		setCursor(tempCursor)
-		console.log("dataUpdated",type)
+		console.log("dataUpdated", type)
 	}
 	const initData = async () => {
-		const type = activeKey==="1"?ARTWORK_TYPE.PURE_TEXT:activeKey==="2"?ARTWORK_TYPE.PHOTO:ARTWORK_TYPE.PAINTING;
+		const type = activeKey === "1" ? ARTWORK_TYPE.PURE_TEXT : activeKey === "2" ? ARTWORK_TYPE.PHOTO : ARTWORK_TYPE.PAINTING;
 		let { cursor: tempCursor, data } = await getArtworkList(type, 'like');
 		setDataList(data)
 		setCursor(tempCursor)
-		console.log("datainited",type)
+		console.log("datainited", type)
 	}
 	const getChildren = () => (
 		<div
@@ -89,9 +94,10 @@ function Post() {
 					dataSource={dataList}
 					renderItem={(item) => (
 						<List.Item key={item.email}>
-							<Card title={item.name} bordered={true} style={{ width: "100%" }}>
+							<Card title={item.name} bordered={true} style={{ width: "100%" }} extra={<Button type='link' onClick={()=>{export2json(item)}}>Download</Button>}>
 								<p>text: {item.text}</p>
 								<p>type: {item.type}</p>
+								<p>create time: {item.createTime}</p>
 							</Card>
 						</List.Item>
 
@@ -118,54 +124,24 @@ function Post() {
 			children: getChildren()
 		},
 	];
-	const onChange = (key:string) => {
-		setActiveKey(key)
+	const onChange = (key: string) => {
+		setActiveKey(key);
 	}
+	function export2json(data:Artwork) {
+		const a = document.createElement("a");
+		a.href = URL.createObjectURL(
+		  new Blob([JSON.stringify(data, null, 2)], {
+		 type: "application/json"
+		  })
+		);
+		a.setAttribute("download", "data.json");
+		document.body.appendChild(a);
+		a.click();
+		document.body.removeChild(a);
+		 }
 	return (
 		<>
-			<Tabs defaultActiveKey="1" items={items} onChange={onChange}/>
-			{cursor ? "還有" : "沒了"}
-			{/* <div
-				id="scrollableDiv"
-				style={{
-					height: 400,
-					overflow: 'auto',
-					padding: '0 16px',
-					border: '1px solid rgba(140, 140, 140, 0.35)',
-				}}
-			>
-
-				<InfiniteScroll
-					dataLength={dataList.length}
-					next={loadMoreData}
-					hasMore={cursor}
-					loader={
-						<Skeleton
-							avatar
-							paragraph={{
-								rows: 1,
-							}}
-							active
-						/>
-					}
-					endMessage={<Divider plain>It is all, nothing more 🤐</Divider>}
-					scrollableTarget="scrollableDiv"
-				>
-					<List
-						dataSource={dataList}
-						renderItem={(item) => (
-							<List.Item key={item.email}>
-								<Card title={item.name} bordered={true} style={{ width: "100%" }}>
-									<p>text: {item.text}</p>
-								</Card>
-							</List.Item>
-
-						)}
-					/>
-				</InfiniteScroll>
-
-			</div> */}
-
+			<Tabs defaultActiveKey={activeKey} items={items} onChange={onChange} />
 		</>
 	);
 }
