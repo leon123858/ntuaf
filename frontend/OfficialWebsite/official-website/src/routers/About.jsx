5263be16a2ff6bfa4ce1f221fe089fa38e120e96
dsa66253 <dsa66253@yahoo.com.tw>
2023-02-17 20:32:29 +0800

    finish draft of murmur

--- frontend/OfficialWebsite/official-website/src/routers/About.jsx
+++ frontend/OfficialWebsite/official-website/src/routers/About.jsx
@@ -1,11 +1,16 @@
 import React from 'react';
 import style from './Home.module.css';
 import MemberTab from '../components/MemberTab/MemberTab'
+import DynamicVision from "../components/DynamicVision/DynamicVision"
+import Murmur from "../components/Murmur/Murmur"
 
 function About() {
 	return (
 		<>
 			<div className={style.APP}>
+				<Murmur/>
+				<DynamicVision/>
+
 				<MemberTab />
 			</div>
 		</>
