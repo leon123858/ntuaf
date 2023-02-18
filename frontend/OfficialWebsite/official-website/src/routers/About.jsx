d1a31dd5c16f6249a00cc4bd47df3ef4467d6348
dsa66253 <dsa66253@yahoo.com.tw>
2023-02-18 11:00:14 +0800

    finish sponsor first draft

--- frontend/OfficialWebsite/official-website/src/routers/About.jsx
+++ frontend/OfficialWebsite/official-website/src/routers/About.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import style from './Home.module.css';
 import MemberTab from '../components/MemberTab/MemberTab'
+import Sponsor from "../components/Sponsor/Sponsor"
 
 function About() {
 	return (
-		<>
 			<div className={style.APP}>
+				<Sponsor/>
 				<MemberTab />
 			</div>
-		</>
 	);
 }
 
