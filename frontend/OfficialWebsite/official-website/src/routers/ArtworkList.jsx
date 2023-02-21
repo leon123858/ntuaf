c351cd9e903bdbb3eeb72934832c1b4bc0890995
jason.lin <jason.lin@access-company.com>
2023-02-21 16:05:48 +0800

    幾乎沒做

--- frontend/OfficialWebsite/official-website/src/routers/ArtworkList.jsx
+++ frontend/OfficialWebsite/official-website/src/routers/ArtworkList.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import style from './ArtworkList.module.css';
-
+import {ArtworkList as List } from '../components/ArtworkList/ArtworkList';
 function ArtworkList() {
 	return (
 		<>
-			<div className={style.APP}>ArtworkList</div>
+			<List/>
 		</>
 	);
 }
