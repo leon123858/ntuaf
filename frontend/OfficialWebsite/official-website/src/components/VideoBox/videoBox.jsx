6759be4246e6a1b8ce2397d65e195d1a7c8465cc
Cedricyu <86541569+Cedricyu@users.noreply.github.com>
2023-03-04 11:49:20 +0800

    fix video bug

--- frontend/OfficialWebsite/official-website/src/components/VideoBox/videoBox.jsx
+++ frontend/OfficialWebsite/official-website/src/components/VideoBox/videoBox.jsx
@@ -3,7 +3,14 @@ import style from './VideoBox.module.css';
 import { Card } from 'antd';
 
 const Video = ({url, text}) => {
-
+	console.log("url =",url);
+	function insertAtIndex(str, substring, index) {
+		return str.slice(0, index) + substring + str.slice(index);
+	}
+	// const video = url;
+	const str = "https://www.youtube.com/";
+	const video = insertAtIndex(url, "embed/", str.length);
+	console.log("url after changed =",video);
 	return (
 		<div className={style.container}>
 			<h1 style={{ marginBottom: 20 }}>回顧片</h1>
@@ -12,7 +19,7 @@ const Video = ({url, text}) => {
 			</Card>
 			<iframe
 				height="480"
-				src={url}
+				src={video}
 				title="YouTube video player"
 				allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture;"
 				allowFullScreen
