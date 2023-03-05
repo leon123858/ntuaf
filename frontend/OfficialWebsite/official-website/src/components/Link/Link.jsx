1a3b889b78a645cf2e7cba0b70538d920ccd866e
leon123858 <a0970785699@gmail.com>
2023-03-05 15:34:13 +0800

    edit coding style and a bug

--- frontend/OfficialWebsite/official-website/src/components/Link/Link.jsx
+++ frontend/OfficialWebsite/official-website/src/components/Link/Link.jsx
@@ -2,17 +2,16 @@ import React from 'react';
 import style from './Link.module.css';
 import { Button } from 'antd';
 
-
 const Link = ({ url, title }) => {
-
-    return (
-        <div className={style.container}>
-            <a href={url} target="_blank">
-                <Button className={style.btn}>{title} {'>'}</Button>
-            </a>
-        </div>
-    );
-}
-
+	return (
+		<div className={style.container}>
+			<a href={url} target='_blank'>
+				<Button className={style.btn}>
+					{title} {'>'}
+				</Button>
+			</a>
+		</div>
+	);
+};
 
 export default Link;
