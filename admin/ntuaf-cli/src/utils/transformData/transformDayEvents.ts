ca184facdba7f84fb2ca92bdbd13acb0927bcdf4
leon123858 <a0970785699@gmail.com>
2023-03-06 00:15:47 +0800

    new SDK cache method

--- admin/ntuaf-cli/src/utils/transformData/transformDayEvents.ts
+++ admin/ntuaf-cli/src/utils/transformData/transformDayEvents.ts
@@ -5,6 +5,7 @@ const { FieldValue } = require('firebase-admin/firestore');
 import { EVENT_TYPE } from '@leon123858/ntuaf-sdk';
 
 const transformDayEvents = async () => {
+	const monthEvent = { '4': {}, '5': {} } as any;
 	const toDbPath = 'Cache/Events/DayEvents';
 	const afStart = moment('04/20/2023', 'MM/DD/YYYY');
 	const afEnd = moment('05/20/2023', 'MM/DD/YYYY');
@@ -35,44 +36,79 @@ const transformDayEvents = async () => {
 		exhibition: [] as any[],
 	};
 	let countL = 0;
-	eventRef.forEach(async (doc) => {
-		const data = doc.data();
-		const startTime = moment(data.startTime, 'x');
-		const endTime = moment(data.endTime, 'x');
-		const handleTime = startTime.clone();
-		for (; ; countL++) {
-			const id = handleTime.format('M_D');
-			// console.log(countL, startTime.format("M/D"), endTime.format("M/D"), startTime.valueOf(),handleTime.valueOf(), endTime.valueOf(),  data.id)
-			if (data.type === EVENT_TYPE.展覽) {
-				await db
-					.collection(toDbPath)
-					.doc(id)
-					.update({
-						'data.activity': FieldValue.arrayUnion({
-							name: data.title,
-							info: data.blocks[0]?.text || '無資料',
-							id: data.id,
-						}),
-					});
-			} else {
-				await db
-					.collection(toDbPath)
-					.doc(id)
-					.update({
-						'data.exhibition': FieldValue.arrayUnion({
-							name: data.title,
-							info: data.blocks[0]?.text || '無資料',
-							id: data.id,
-						}),
-					});
+	await Promise.all(
+		eventRef.docs.map(async (doc) => {
+			const data = doc.data();
+			const startTime = moment(data.startTime, 'x');
+			const endTime = moment(data.endTime, 'x');
+			const handleTime = startTime.clone();
+			for (; ; countL++) {
+				const id = handleTime.format('M_D');
+				const month = handleTime.format('M');
+				const eventType = data.type;
+				if (monthEvent[month][id] == undefined) {
+					monthEvent[month][id] = [false, false, false];
+				}
+				switch (eventType) {
+					case EVENT_TYPE.展覽:
+						monthEvent[month][id][2] = true;
+						break;
+					case EVENT_TYPE.工作坊:
+					case EVENT_TYPE.講座:
+						monthEvent[month][id][1] = true;
+						break;
+					default:
+						monthEvent[month][id][0] = true;
+						break;
+				}
+				// console.log(countL, startTime.format("M/D"), endTime.format("M/D"), startTime.valueOf(),handleTime.valueOf(), endTime.valueOf(),  data.id)
+				if (eventType === EVENT_TYPE.展覽) {
+					await db
+						.collection(toDbPath)
+						.doc(id)
+						.update({
+							'data.activity': FieldValue.arrayUnion({
+								name: data.title,
+								info: data.blocks[0]?.text || '無資料',
+								id: data.id,
+							}),
+						});
+				} else {
+					await db
+						.collection(toDbPath)
+						.doc(id)
+						.update({
+							'data.exhibition': FieldValue.arrayUnion({
+								name: data.title,
+								info: data.blocks[0]?.text || '無資料',
+								id: data.id,
+							}),
+						});
+				}
+				handleTime.add(1, 'days');
+				// console.log(handleTime.valueOf())
+				if (handleTime.valueOf() >= endTime.valueOf()) {
+					break;
+				}
 			}
-			handleTime.add(1, 'days');
-			// console.log(handleTime.valueOf())
-			if (handleTime.valueOf() >= endTime.valueOf()) {
-				break;
-			}
-		}
-	});
+		})
+	);
+	await db
+		.collection('Cache/Events/MonthEvents')
+		.doc('4')
+		.set({
+			data: {
+				...monthEvent['4'],
+			},
+		});
+	await db
+		.collection('Cache/Events/MonthEvents')
+		.doc('5')
+		.set({
+			data: {
+				...monthEvent['5'],
+			},
+		});
 };
 
 export { transformDayEvents };
--- middleware/ntuaf-sdk/src/utils/db/cache.ts
+++ middleware/ntuaf-sdk/src/utils/db/cache.ts
@@ -79,6 +79,31 @@ export const getDayEvents = async (
 };
 
 /**
+ * 根據月份取得各天有哪些類別的事件
+ * @param month
+ * @returns
+ * @example
+ * // 獲取 5 月的事件類別列表
+ * await getMonthsEventsType(5) // {'5_1':[true,false,true],'5_2':[false,true,false],...}
+ * // '5_1':[true,false,true] 表示 5月1日 有一般活動, 沒講座工作坊, 有常設展覽
+ * // '5_2':[false,true,false] 表示 5月2日 沒一般活動, 有講座工作坊, 沒常設展覽
+ */
+export const getMonthsEventsType = async (month: '4' | '5') => {
+	// ex: Cache/Events/MonthEvents/4
+	const data = (
+		await getDoc(doc(dbInstance, 'Cache/Events/MonthEvents', `${month}`))
+	).data() as
+		| {
+				data: {
+					[key: string]: [boolean, boolean, boolean];
+				};
+		  }
+		| undefined;
+	if (!data) throw 'can not fetch target cache';
+	return data.data;
+};
+
+/**
  * 取得該大類的事件介紹
  * @param tabName
  * @returns
