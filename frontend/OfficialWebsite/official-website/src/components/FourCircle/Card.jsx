17123d59fc7df0f1eb81ee2776be2aa043d9a42c
leon123858 <a0970785699@gmail.com>
2023-03-05 13:09:42 +0800

    debug for 4 ball

--- frontend/OfficialWebsite/official-website/src/components/FourCircle/Card.jsx
+++ frontend/OfficialWebsite/official-website/src/components/FourCircle/Card.jsx
@@ -1,25 +1,42 @@
-import styles from "./Card.module.css"
+import styles from './Card.module.css';
 import { Modal } from 'antd';
 import { useState } from 'react';
-import DoubleCard from "./DoubleCard.jsx"
+import DoubleCard from './DoubleCard.jsx';
 const Card = ({ content }) => {
-    const [isModalOpen, setIsModalOpen] = useState(false);
-    const showModal = () => {
-        setIsModalOpen(true);
-    };
-    const handleCancel = () => {
-        setIsModalOpen(prev => false);
-    };
-    return (
-        <div className={styles.cardWrapper}>
-            <div className={styles.circle}>
-                <div className={styles.circleText} onClick={showModal}>{content.title}</div>
-            </div>
-            <Modal title="Basic Modal" style={{pointerEvents:"auto"}} open={isModalOpen} onCancel={handleCancel} modalRender={()=><DoubleCard FrontCardContent={content} BackCardContent={content} closeModel={handleCancel}/>}>
-                <p>{content.title}</p>
-            </Modal>
-        </div>)
-}
+	const [isModalOpen, setIsModalOpen] = useState(false);
+	const showModal = () => {
+		setIsModalOpen(true);
+	};
+	const handleCancel = () => {
+		setIsModalOpen((prev) => false);
+	};
+	return (
+		<div className={styles.cardWrapper}>
+			<div
+				className={styles.circle}
+				onClick={() => {
+					showModal();
+				}}
+			>
+				<div className={styles.circleText}>{content.title}</div>
+			</div>
+			<Modal
+				title='Basic Modal'
+				style={{ pointerEvents: 'auto' }}
+				open={isModalOpen}
+				onCancel={handleCancel}
+				modalRender={() => (
+					<DoubleCard
+						FrontCardContent={content}
+						BackCardContent={content}
+						closeModel={handleCancel}
+					/>
+				)}
+			>
+				<p>{content.title}</p>
+			</Modal>
+		</div>
+	);
+};
 
-
-export default Card
+export default Card;
