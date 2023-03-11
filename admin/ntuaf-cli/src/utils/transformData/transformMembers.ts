0510e444d3339fed7f5b651611b9d8bf6043b68d
Mary <dsa66253@yahoo.com.tw>
2023-03-11 14:56:22 +0800

    finish

--- admin/ntuaf-cli/src/utils/transformData/transformMembers.ts
+++ admin/ntuaf-cli/src/utils/transformData/transformMembers.ts
@@ -4,65 +4,29 @@ const transformMembers = async () => {
 	console.log('transformMembersByDepartment');
 	const key: string = 'data';
 	const ref = await db.collection('Members').get();
-	const init = async ()=>{
-		ref.forEach(async (doc) => {
-			// initialize each department list
-			const department = doc.data().department;
-			const docRef = db.collection('Cache/Members/Department').doc(department);
-			await docRef.set({ [key]: [] });
-			console.log("init ", department)
-			return
-		})
-		return
-	}
-	// await init()
+	
+	// init list
 	for (let doc of ref.docs){
 		const department = doc.data().department;
 		const docRef = db.collection('Cache/Members/Department').doc(department);
 		await docRef.set({ [key]: [] });
 		console.log("init ", department)
 	}
-	for (let doc of ref.docs){
+	// fill list
+	await Promise.all(ref.docs.map(async doc=> {
 		const department = doc.data().department;
-			const docRef = db.collection('Cache/Members/Department').doc(department);
-			await docRef.update({
-				[key]: FieldValue.arrayUnion({
-					name: doc.data().name,
-					job: doc.data().job,
-				}),
-			});
+		const docRef = db.collection('Cache/Members/Department').doc(department);
+		docRef.update({
+			[key]: FieldValue.arrayUnion({
+				name: doc.data().name,
+				job: doc.data().job,
+			}),
+		});
 		console.log("insert ", doc.data().name)
-	}
-	const init2 = ()=>{
-		for (let doc in ref){
-			console.log(doc)
-		}
-	}
+	}))
+
 	// init2()
-	const createCache = async ()=>{
-		for(let doc of ref.docs){
-			const department = doc.data().department;
-			const docRef = db.collection('Cache/Members/Department').doc(department);
-			await docRef.update({
-				[key]: FieldValue.arrayUnion({
-					name: doc.data().name,
-					job: doc.data().job,
-				}),
-			});
-			console.log("insert ", doc.data().name)
-		}
-		await Promise.all(ref.docs.map(async doc=> {
-			const department = doc.data().department;
-			const docRef = db.collection('Cache/Members/Department').doc(department);
-			docRef.update({
-				[key]: FieldValue.arrayUnion({
-					name: doc.data().name,
-					job: doc.data().job,
-				}),
-			});
-		}))
-	}
-	// await createCache()
+	
 	
 };
 export { transformMembers };
