7d3f46817dc638507fd4c04639cb11a30745b479
leon123858 <a0970785699@gmail.com>
2023-03-11 22:01:31 +0800

    confirm set background for diff router

--- frontend/OfficialWebsite/official-website/src/components/Layout/Layout.jsx
+++ frontend/OfficialWebsite/official-website/src/components/Layout/Layout.jsx
@@ -3,13 +3,19 @@ import Header from '../Header/Header';
 import { Outlet } from 'react-router-dom';
 import styles from './Layout.module.css';
 import Footer from '../Footer/Footer';
+import { useLocation } from 'react-router-dom';
+
+const LAYOUT_BACKGROUND_IMAGE = {
+	'/calendar': '/background/c1.png',
+};
 
 const Layout = () => {
+	const location = useLocation();
 	return (
 		<div
 			className={styles.layout}
 			style={{
-				backgroundImage: "url('/background/c1.png')",
+				backgroundImage: `url('${LAYOUT_BACKGROUND_IMAGE[location.pathname]}')`,
 				backgroundSize: '100vw 100vh',
 			}}
 		>
