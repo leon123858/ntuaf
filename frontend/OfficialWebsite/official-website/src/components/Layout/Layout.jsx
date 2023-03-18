e3c5ac51a8929c9a8d13c5ab53890ee82611e5fe
leon123858 <a0970785699@gmail.com>
2023-03-18 12:08:07 +0800

    update event list background

--- frontend/OfficialWebsite/official-website/src/components/Layout/Layout.jsx
+++ frontend/OfficialWebsite/official-website/src/components/Layout/Layout.jsx
@@ -1,28 +1,49 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import Header from '../Header/Header';
 import { Outlet } from 'react-router-dom';
 import styles from './Layout.module.css';
 import Footer from '../Footer/Footer';
 import { useLocation } from 'react-router-dom';
+import { BreakPointContext } from '../../useBreakPoint';
 
 const LAYOUT_BACKGROUND_IMAGE = {
 	'/calendar': '/background/c1.png',
+	'/introduce/exhibition': {
+		web: '/background/iWebB.png',
+		mobile: '/background/iMobileB.png',
+	},
+};
+
+const getBackgroundImage = (path, inBreakPoint) => {
+	if (typeof LAYOUT_BACKGROUND_IMAGE[path] == 'string') {
+		return LAYOUT_BACKGROUND_IMAGE[path];
+	}
+	return inBreakPoint
+		? LAYOUT_BACKGROUND_IMAGE[path].mobile
+		: LAYOUT_BACKGROUND_IMAGE[path].web;
 };
 
 const Layout = () => {
 	const location = useLocation();
+	const { inBreakPoint } = useContext(BreakPointContext);
 	return (
-		<div
-			className={styles.layout}
-			style={{
-				backgroundImage: `url('${LAYOUT_BACKGROUND_IMAGE[location.pathname]}')`,
-				backgroundSize: '100vw 100vh',
-			}}
-		>
+		<>
 			<Header />
-			<Outlet />
-			<Footer />
-		</div>
+			<div
+				className={styles.layout}
+				style={{
+					backgroundImage: `url('${getBackgroundImage(
+						location.pathname,
+						inBreakPoint
+					)}')`,
+					backgroundSize: '100vw 100vh',
+					backgroundAttachment: 'fixed',
+				}}
+			>
+				<Outlet />
+				<Footer />
+			</div>
+		</>
 	);
 };
 export default Layout;
