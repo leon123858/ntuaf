d621ff444d21f3646c2b01a8998f233e0c8545fe
leon123858 <a0970785699@gmail.com>
2023-03-18 12:33:52 +0800

    fix name file bug

--- frontend/OfficialWebsite/official-website/src/components/ArtBox/ArtBoxText.jsx
+++ frontend/OfficialWebsite/official-website/src/components/ArtBox/ArtBoxText.jsx
@@ -1,68 +1,66 @@
-import styles from "./artBox.module.css"
-import React, { useState } from "react";
-import { Avatar, Card ,Image, Button} from 'antd';
-import Meta from "antd/es/card/Meta";
-import Heart from "./heart"
-import ReactTextCollapse from "react-text-collapse";
+import styles from './ArtBox.module.css';
+import React, { useState } from 'react';
+import { Avatar, Card, Image, Button } from 'antd';
+import Meta from 'antd/es/card/Meta';
+import Heart from './Heart';
+import ReactTextCollapse from 'react-text-collapse';
 
 const TEXT_COLLAPSE_OPTIONS = {
-    collapse: false, // default state when component rendered
-    collapseText: '...', // text to show when collapsed
-    expandText: 'show less', // text to show when expanded
-    minHeight: 100, // component height when closed
-    maxHeight: 250,// expanded to
-    textStyle: { // pass the css for the collapseText and expandText here
-      color: "blue",
-      fontSize: "20px"
-    }
-}
+	collapse: false, // default state when component rendered
+	collapseText: '...', // text to show when collapsed
+	expandText: 'show less', // text to show when expanded
+	minHeight: 100, // component height when closed
+	maxHeight: 250, // expanded to
+	textStyle: {
+		// pass the css for the collapseText and expandText here
+		color: 'blue',
+		fontSize: '20px',
+	},
+};
 
-const ArtBox = (props)=>{
-    const [active, setActive] = useState(false)
-    console.log(props);
-    const mystyle = {
-        width : `${props.width}%`
-    }
-    return(
-        <div className={styles.App}>
-            <Card
-            style={mystyle}
-            hoverable
-            >
-            <Meta
-                title={props.artName}
-                // description={props.artist}
-                style={{
-                    width: 100,
-                }}
-                className={styles.left}
-            >
-            
-            </Meta>
-            <div className={styles.text}>
-                <ReactTextCollapse options={TEXT_COLLAPSE_OPTIONS} className={styles.text}>
-                    <p>
-                    {props.article}
-                    </p>
-                </ReactTextCollapse>
-            </div>
-                <div className={styles.container}>
-                    <Meta
-                        description={props.artist}
-                        style={{
-                            width: 100,
-                        }}
-                    >
-                    </Meta>
-                    <Heart
-                    style={{
-                        width: 50,
-                    }}
-                    isActive={active} onClick={() => setActive(!active)}/>
-                </div>
-            </Card>
-        </div>
-    )
-}
+const ArtBox = (props) => {
+	const [active, setActive] = useState(false);
+	console.log(props);
+	const mystyle = {
+		width: `${props.width}%`,
+	};
+	return (
+		<div className={styles.App}>
+			<Card style={mystyle} hoverable>
+				<Meta
+					title={props.artName}
+					// description={props.artist}
+					style={{
+						width: 100,
+					}}
+					className={styles.left}
+				></Meta>
+				<div className={styles.text}>
+					<ReactTextCollapse
+						options={TEXT_COLLAPSE_OPTIONS}
+						className={styles.text}
+					>
+						<p>{props.article}</p>
+					</ReactTextCollapse>
+				</div>
+				<div className={styles.container}>
+					<Meta
+						description={props.artist}
+						style={{
+							width: 100,
+						}}
+					></Meta>
+					<Heart
+						style={{
+							width: 50,
+						}}
+						isActive={active}
+						onClick={() => setActive(!active)}
+					/>
+				</div>
+			</Card>
+		</div>
+	);
+};
 
 export default ArtBox;
