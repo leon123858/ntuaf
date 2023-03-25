ef06bd7d4d8edbf1522daed392ec0dd230903fdb
leon123858 <a0970785699@gmail.com>
2023-03-25 11:44:42 +0800

    Revert "update 4ball"
    This reverts commit a2a0550d87fbd9d67e0d085c889b3a6d469bf042.

--- frontend/OfficialWebsite/official-website/src/components/FourCircle/DoubleCard.jsx
+++ frontend/OfficialWebsite/official-website/src/components/FourCircle/DoubleCard.jsx
@@ -1,77 +1,30 @@
 // import OneSideCard from "./OneSideCard.jsx"
-import style from './DoubleCard.module.css';
-import FrontCard from './FrontCard.jsx';
-import BackCard from './BackCard.jsx';
-import { useContext } from 'react';
+import style from "./DoubleCard.module.css"
+import FrontCard from "./FrontCard.jsx"
+import BackCard from "./BackCard.jsx"
+import { useContext } from "react"
 import { BreakPointContext } from '../../useBreakPoint';
 
-const DoubleSideCard = ({
-	FrontCardContent,
-	BackCardContent,
-	closeModel,
-	flip,
-	setFlip,
-}) => {
-	const { inBreakPoint } = useContext(BreakPointContext);
-	return (
-		<>
-			{inBreakPoint ? (
-				<div className={style.lgDCardWrapper}>
-					<div
-						className={
-							flip === true
-								? [style.dCardInnerWrapper, style.dCardInnerWrapperFlip].join(
-										' '
-								  )
-								: style.dCardInnerWrapper
-						}
-					>
-						<div className={style.flipFrontCard}>
-							<FrontCard
-								FrontCardContent={FrontCardContent}
-								setFlip={setFlip}
-								closeModel={closeModel}
-							/>
-						</div>
-						<div className={style.flipBackCard}>
-							<BackCard
-								BackCardContent={BackCardContent}
-								setFlip={setFlip}
-								closeModel={closeModel}
-							/>
-						</div>
-					</div>
-				</div>
-			) : (
-				<div className={style.dCardWrapper}>
-					<div
-						className={
-							flip === true
-								? [style.dCardInnerWrapper, style.dCardInnerWrapperFlip].join(
-										' '
-								  )
-								: style.dCardInnerWrapper
-						}
-					>
-						<div className={style.flipFrontCard}>
-							<FrontCard
-								FrontCardContent={FrontCardContent}
-								setFlip={setFlip}
-								closeModel={closeModel}
-							/>
-						</div>
-						<div className={style.flipBackCard}>
-							<BackCard
-								BackCardContent={BackCardContent}
-								setFlip={setFlip}
-								closeModel={closeModel}
-							/>
-						</div>
-					</div>
-				</div>
-			)}
-		</>
-	);
-};
+const DoubleSideCard = ({FrontCardContent, BackCardContent, closeModel, flip, setFlip}) => {
+    const { inBreakPoint } = useContext(BreakPointContext);
+    return (
+        <>{
+            inBreakPoint?
+            <div className={style.lgDCardWrapper}>
+                <div className={flip===true?[style.dCardInnerWrapper, style.dCardInnerWrapperFlip].join(' '):style.dCardInnerWrapper}>
+                    <div className={style.flipFrontCard}><FrontCard FrontCardContent={FrontCardContent} setFlip={setFlip} closeModel={closeModel}/></div>
+                    <div className={style.flipBackCard}><BackCard BackCardContent={BackCardContent} setFlip={setFlip} closeModel={closeModel}/></div>
+                </div>
+            </div>:
+            <div className={style.dCardWrapper}>
+                <div className={flip===true?[style.dCardInnerWrapper, style.dCardInnerWrapperFlip].join(' '):style.dCardInnerWrapper}>
+                    <div className={style.flipFrontCard}><FrontCard FrontCardContent={FrontCardContent} setFlip={setFlip} closeModel={closeModel}/></div>
+                    <div className={style.flipBackCard}><BackCard BackCardContent={BackCardContent} setFlip={setFlip} closeModel={closeModel}/></div>
+                </div>
+            </div>
+        }</>
 
-export default DoubleSideCard;
+    )
+}
+
+export default DoubleSideCard
