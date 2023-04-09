5da8017a0a1149455e80b2b1f0bfc00e5b2d64f9
leon123858 <a0970785699@gmail.com>
2023-04-09 14:07:56 +0800

    add video

--- frontend/OfficialWebsite/official-website/src/components/DynamicVision/DynamicVision.jsx
+++ frontend/OfficialWebsite/official-website/src/components/DynamicVision/DynamicVision.jsx
@@ -1,13 +1,23 @@
-import styles from "./DynamicVision.module.css"
-import { useContext } from "react"
-import { BreakPointContext } from "../../useBreakPoint"
+import styles from './DynamicVision.module.css';
+import { useContext } from 'react';
+import { BreakPointContext } from '../../useBreakPoint';
 const DynamicVision = () => {
-    const { inBreakPoint} = useContext(BreakPointContext)
-    return (
-        <div className={styles.Wrapper}>
-            <img src={inBreakPoint?"../../about/環-手機版.svg":"../../about/環.svg"} alt="" />
-
-        </div>
-    )
-}
-export default DynamicVision
+	const { inBreakPoint } = useContext(BreakPointContext);
+	return (
+		<div className={styles.Wrapper}>
+			<img
+				src={inBreakPoint ? '../../about/環-手機版.svg' : '../../about/環.svg'}
+				alt=''
+			/>
+			{/* <video
+				src={'/video.mp4'}
+				width={'100%'}
+				autoPlay
+				muted
+				preload
+				loop
+			></video> */}
+		</div>
+	);
+};
+export default DynamicVision;
