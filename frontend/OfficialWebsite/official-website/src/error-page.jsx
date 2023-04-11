e3911d286bcc55dde2c2acf4b8cc763415003639
leon123858 <a0970785699@gmail.com>
2023-04-12 05:47:48 +0800

    add error page

--- frontend/OfficialWebsite/official-website/src/error-page.jsx
+++ frontend/OfficialWebsite/official-website/src/error-page.jsx
@@ -1,4 +1,5 @@
 import { useRouteError } from 'react-router-dom';
+import style from './error-page.module.css';
 
 export default function ErrorPage() {
 	const error = useRouteError();
@@ -6,11 +7,48 @@ export default function ErrorPage() {
 
 	return (
 		<div id='error-page'>
-			<h1>Oops!</h1>
-			<p>Sorry, an unexpected error has occurred.</p>
-			<p>
-				<i>{error.statusText || error.message}</i>
-			</p>
+			<section class={style.page_404}>
+				<div>
+					<div>
+						<div>
+							<div>
+								<div class={style.four_zero_four_bg}>
+									<h1>{error.status || '500'}</h1>
+								</div>
+
+								<div class={style.contant_box_404}>
+									{error.statusText && <h3 class='h2'>{error.statusText}</h3>}
+
+									{error.status ? (
+										<p>{error.message}</p>
+									) : (
+										<>
+											<p>您似乎觸發了未知錯誤</p>
+											{error.message && <p>錯誤訊息: {error.message}</p>}
+											<p>
+												歡迎到我們的
+												<a
+													style={{ color: 'blue' }}
+													href='https://github.com/leon123858/ntuaf/issues'
+												>
+													{' '}
+													github{' '}
+												</a>
+												進行錯誤回報
+											</p>
+											<p>或是 email 至 a0970785699@gmail.com 描述問題</p>
+										</>
+									)}
+
+									<a href='/' class={style.link_404}>
+										回首頁
+									</a>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</section>
 		</div>
 	);
 }
