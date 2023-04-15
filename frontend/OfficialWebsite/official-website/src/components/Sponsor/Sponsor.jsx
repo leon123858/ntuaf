bede8862575acacc3f7b11b0c0be8d9b3dd04a47
ZiJson <jasonnosajya53@gmail.com>
2023-04-15 11:43:17 +0800

    sponsor

--- frontend/OfficialWebsite/official-website/src/components/Sponsor/Sponsor.jsx
+++ frontend/OfficialWebsite/official-website/src/components/Sponsor/Sponsor.jsx
@@ -9,11 +9,11 @@ const Sponsor = () => {
             <div className={styles.sponsorWrapper}>
                 <div className={styles.title}>合辦單位 / 贊助單位</div>
                 <div className={styles.iconsWrapper}>
-                    <img src="../../about/sponsor/台大系統.png" alt="" />
+                    <img src="../../about/sponsor/台大系統.png" alt="" className={styles.ntu}/>
                     <img src="../../about/sponsor/惜惜康普.png" alt="" className={styles.cc}/>
-                    <img src="../../about/sponsor/麗陽LOGO 2.png" alt="" />
-                    <img src="../../about/sponsor/華南銀行.png" alt="" />
-                    <img src="../../about/sponsor/寶貝.png" alt="" />
+                    <img src="../../about/sponsor/麗陽LOGO 2.png" alt="" className={styles.lee}/>
+                    <img src="../../about/sponsor/華南銀行.png" alt="" className={styles.bank}/>
+                    <img src="../../about/sponsor/寶貝.png" alt=""  className={styles.bb}/>
                 </div>
             </div>
         </div>
