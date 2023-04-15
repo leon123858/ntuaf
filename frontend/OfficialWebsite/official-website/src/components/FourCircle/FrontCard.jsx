50b75fcbe729ff9d3a464e0a72038ec5dd45d671
dsa66253 <dsa66253@yahoo.com.tw>
2023-04-15 16:55:18 +0800

    move svg to public instead of url

--- frontend/OfficialWebsite/official-website/src/components/FourCircle/FrontCard.jsx
+++ frontend/OfficialWebsite/official-website/src/components/FourCircle/FrontCard.jsx
@@ -12,10 +12,10 @@ const FrontCard = ({ FrontCardContent, setFlip, closeModel}) => {
     return (
 <>{
     inBreakPoint?
-    <div className={styles.frontCardWrapper} style={{backgroundImage:`url("${FrontCardContent.frontCardBackgroundUrl}")` }}>
+    <div className={styles.frontCardWrapper} style={{backgroundImage:`url("${FrontCardContent.frontCardBackgroundUrl}")` }}  onClick={handleFlip}>
         <div className={styles.title} style={{backgroundImage:`url("${FrontCardContent.iconTextUrl}")` }} ></div>
         <div className={styles.discription}>{FrontCardContent.discription}</div>
-        <div className={styles.flipButton} onClick={handleFlip}>
+        <div className={styles.flipButton}>
         <Image src={"https://drive.google.com/uc?export=view&id=1kMG-u5ilEWa3i2_CSuxAEP9xS2JVekDl"} alt={"loading"}
                     preview={false}
                     fallback='https://images.squarespace-cdn.com/content/v1/5452d441e4b0c188b51fef1a/1615326541809-TW01PVTOJ4PXQUXVRLHI/male-orange-tabby-cat.jpg?format=600w'
@@ -24,10 +24,10 @@ const FrontCard = ({ FrontCardContent, setFlip, closeModel}) => {
         </div>
 
     </div>:
-    <div className={styles.lgFrontCardWrapper} style={{backgroundImage:`url("${FrontCardContent.frontCardBackgroundUrl}")` }}>
+    <div className={styles.lgFrontCardWrapper} style={{backgroundImage:`url("${FrontCardContent.frontCardBackgroundUrl}")` }}  onClick={handleFlip}>
         <div className={styles.lgTitle} style={{backgroundImage:`url("${FrontCardContent.iconTextUrl}")` }} ></div>
         <div className={styles.discription}>{FrontCardContent.discription}</div>
-        <div className={styles.flipButton} onClick={handleFlip}>
+        <div className={styles.flipButton}>
         <Image src={"https://drive.google.com/uc?export=view&id=1kMG-u5ilEWa3i2_CSuxAEP9xS2JVekDl"} alt={"loading"}
                     preview={false}
                     fallback='https://images.squarespace-cdn.com/content/v1/5452d441e4b0c188b51fef1a/1615326541809-TW01PVTOJ4PXQUXVRLHI/male-orange-tabby-cat.jpg?format=600w'
