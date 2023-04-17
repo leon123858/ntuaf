3f24597397b4644f3dd7f614b5cf2756b73a9388
leon123858 <a0970785699@gmail.com>
2023-04-18 02:00:09 +0800

    format

--- frontend/OfficialWebsite/official-website/src/routers/Introduce.jsx
+++ frontend/OfficialWebsite/official-website/src/routers/Introduce.jsx
@@ -7,7 +7,6 @@ import EventList from '../components/EventList/EventList';
 import EventListEx from '../components/EventList/EventListEx';
 import { getTabEvents } from '@leon123858/ntuaf-sdk';
 import { BreakPointContext } from '../useBreakPoint';
-import Hr from '../components/Hr/Hr';
 
 function Introduce() {
 	const { type = 1 } = useParams();
@@ -56,7 +55,8 @@ function Introduce() {
 			<br />
 			<br />
 			<br />
-			<br /><br />
+			<br />
+			<br />
 			<Tabs
 				// className={style.container}
 				animated={false}
@@ -68,22 +68,38 @@ function Introduce() {
 						label:
 							key === '1' ? (
 								<>
-									<h5 style={{ color: '#000000', fontSize: inBreakPoint?"16px":"18px", paddingBottom: '5px' ,letterSpacing:"10px"}}>
+									<h5
+										style={{
+											color: '#000000',
+											fontSize: inBreakPoint ? '16px' : '18px',
+											paddingBottom: '5px',
+											letterSpacing: '10px',
+										}}
+									>
 										{'展覽'}
 									</h5>
 									<div>
 										<hr
 											style={{
-												background: 'linear-gradient(to left, #A9CF59, #E73273, #25499D)',
+												background:
+													'linear-gradient(to left, #A9CF59, #E73273, #25499D)',
 												height: '2px',
 												border: '0px',
-												width : '85%',
+												width: '85%',
 											}}
 										/>
 									</div>
 								</>
 							) : (
-								<h5 style={{ letterSpacing: '10px', fontSize: inBreakPoint? '16px': '18px', paddingBottom: '5px' }}>展覽</h5>
+								<h5
+									style={{
+										letterSpacing: '10px',
+										fontSize: inBreakPoint ? '16px' : '18px',
+										paddingBottom: '5px',
+									}}
+								>
+									展覽
+								</h5>
 							),
 						children:
 							firstData.events.length === 0 ? (
@@ -99,22 +115,38 @@ function Introduce() {
 						label:
 							key === '2' ? (
 								<>
-									<h5 style={{ color: '#000000', fontSize: inBreakPoint?"16px":"18px", paddingBottom: '5px' ,letterSpacing:"10px"}}>
+									<h5
+										style={{
+											color: '#000000',
+											fontSize: inBreakPoint ? '16px' : '18px',
+											paddingBottom: '5px',
+											letterSpacing: '10px',
+										}}
+									>
 										{'活動'}
 									</h5>
 									<div>
 										<hr
 											style={{
-												background: 'linear-gradient(to left, #A9CF59, #E73273, #25499D)',
+												background:
+													'linear-gradient(to left, #A9CF59, #E73273, #25499D)',
 												height: '2px',
 												border: '0px',
-												width : '85%',
+												width: '85%',
 											}}
 										/>
 									</div>
 								</>
 							) : (
-								<h5 style={{ letterSpacing: '10px', fontSize: inBreakPoint? '16px': '18px', paddingBottom: '5px' }}>活動</h5>
+								<h5
+									style={{
+										letterSpacing: '10px',
+										fontSize: inBreakPoint ? '16px' : '18px',
+										paddingBottom: '5px',
+									}}
+								>
+									活動
+								</h5>
 							),
 						children:
 							secondData.events.length === 0 ? (
