6a5b897714902aea25c9be4c81925204049b1861
leon123858 <a0970785699@gmail.com>
2023-04-26 11:44:32 +0800

    update error page

--- frontend/OfficialWebsite/official-website/src/error-page.jsx
+++ frontend/OfficialWebsite/official-website/src/error-page.jsx
@@ -18,27 +18,23 @@ export default function ErrorPage() {
 
 								<div class={style.contant_box_404}>
 									{error.statusText && <h3 class='h2'>{error.statusText}</h3>}
-
-									{error.status ? (
-										<p>{error.message}</p>
-									) : (
-										<>
-											<p>您似乎觸發了未知錯誤</p>
-											{error.message && <p>錯誤訊息: {error.message}</p>}
-											<p>
-												歡迎到我們的
-												<a
-													style={{ color: 'blue' }}
-													href='https://github.com/leon123858/ntuaf/issues'
-												>
-													{' '}
-													github{' '}
-												</a>
-												進行錯誤回報
-											</p>
-											<p>或是傳訊至台大藝術季臉書粉絲專頁描述問題</p>
-										</>
-									)}
+									<>
+										{error.message && <p>錯誤訊息: {error.message}</p>}
+										<p>
+											若有相關建議或問題, 歡迎到我們的{' '}
+											<a
+												style={{
+													color: 'blue',
+													textDecorationLine: 'underline',
+												}}
+												href='https://github.com/leon123858/ntuaf/issues'
+											>
+												github
+											</a>{' '}
+											發起議題 "issue"
+										</p>
+										<p>或是傳訊至台大藝術季臉書粉絲專頁描述問題</p>
+									</>
 
 									<a href='/' class={style.link_404}>
 										回首頁
